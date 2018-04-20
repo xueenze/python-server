c26c3019d35c26db2c95d4cafd4e084011cd047f
enzexue <kameleon@126.com>
2018-04-20 14:39:12 +0800

    增加测试接口JSON返回

--- wxcrow/app.py
+++ wxcrow/app.py
@@ -1,3 +1,4 @@
+import utils
 import json
 from flask import Flask
 
@@ -6,18 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = {
-        'code': 0,
-        'data': [{
-            'id': 1,
-            'imageUrl': '/images/card/1.jpg',
-            'title': '【微信跳一跳】浅析（上）',
-            'subTitle': '微信小游戏',
-            'addDate': '2017-12-30',
-            'redirctUrl': '/pages/detail/analysis/smallgame1/smallgame1'
-            }]
-    }
+    data = utils.transformJSON('mock/index.json')
     return json.dumps(data, ensure_ascii=False)
-
 if __name__ == '__main__':
     app.run(debug=True)
